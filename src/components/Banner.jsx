358ebdf642927d0df92b2518b431d5f9dee2613b
Tadeo <teoleon2013@gmail.com>
2024-07-03 12:30:35 -0300

    Falta contenido del footer y las rutas

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ export const Banner = () => {
           alt="Banner Immage"
           className="img-fluid  p-0"
         />
-        <h1 className="position-absolute text-white p-0 my-3 mx-3 text-start">
+        <h1 className="position-absolute text-white p-1 my-3 mx-3 text-start">
           <p className="fs-1 ">Poder Judicial de la Nación</p>
           <p className="fs-3">Soporte Master</p>
         </h1>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
 export const Footer = () => {
   return (
-    <div className="container" style={{ backgroundColor: "rgb(38 67 195)" }}>
-      Footer
+    <div
+      className="container-fluid p-2 w-100"
+      style={{
+        backgroundColor: "rgb(38 67 195)",
+        position: "absolute",
+        bottom: "0",
+        height: "350px",
+      }}
+    >
+      Footerr
     </div>
   );
 };
